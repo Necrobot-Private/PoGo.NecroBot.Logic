7de943a016e8de57eeb99425c849737f0c6c581f
TheWizard1328 <the.wizard@live.ca>
2017-06-30 22:46:18 -0600

    Minor code changes/formatting
    Replaced , with | in Translations.EventFortUsed
    Added Lvl in Translations.EventPokemonTransferred
    Added Level in TransferPokemonEvents
    Added LEvel to TransferPokemonTask
    Added LEvel to BaseTransferPokemonTask
    Added extra padding space to Move1/2 in pokemonCaptureEvent

--- Common/Translations.cs
+++ Common/Translations.cs
@@ -350,7 +350,7 @@ namespace PoGo.NecroBot.Logic.Common
                 new KeyValuePair<TranslationString, string>(TranslationString.FarmPokestopsNoUsableFound,
                     "No usable PokeStops found in your area. Is your maximum distance too small?"),
                 new KeyValuePair<TranslationString, string>(TranslationString.EventFortUsed,
-                    "Name: {0} XP: {1}, Gems: {2}, Items: {3}, Eggs: {4}, Lat: {5}, Long: {6}, Alt: {7}"),
+                    "Name: {0} | XP: {1} | Gems: {2} | Items: {3} | Eggs: {4} | Lat: {5} Long: {6} | Alt: {7}"),
                 new KeyValuePair<TranslationString, string>(TranslationString.EventFortFailed,
                     "Name: {0} INFO: Looting failed, possible softban. Unban in: {1}/{2}"),
                 new KeyValuePair<TranslationString, string>(TranslationString.Gym, "Gym"),
@@ -367,7 +367,7 @@ namespace PoGo.NecroBot.Logic.Common
                 new KeyValuePair<TranslationString, string>(TranslationString.EventPokemonEvolvedFailed,
                     "Failed {0}. Result was {1}, stopping evolving {2}"),
                 new KeyValuePair<TranslationString, string>(TranslationString.EventPokemonTransferred,
-                    "{0} | CP: {1}/{3} | IV: {2}%/{4}% | Candies: {5}"),
+                    "{0} | CP: {1}/{3} | IV: {2}%/{4}% | Candies: {5} | Lvl: {6}"),
                 new KeyValuePair<TranslationString, string>(TranslationString.EventPokemonUpgraded,
                     "{0} | CP: {2}/{4} | IV: {3}%/{5}% | SD: {6} | Candies: {7} | Lvl: {1}"),
 

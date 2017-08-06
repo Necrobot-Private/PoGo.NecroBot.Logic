454b2f3d74f795a9c67d904dcd4b657bd138670d
TheWizard1328 <the.wizard@live.ca>
2017-08-06 00:13:37 -0600

    Added Logger Line for "All accounts are Blocked"

--- MultiAccountManager.cs
+++ MultiAccountManager.cs
@@ -290,6 +290,7 @@ namespace PoGo.NecroBot.Logic
             // If we got here all accounts blocked so pause and retry.
             var pauseTime = session.LogicSettings.MultipleBotConfig.OnLimitPauseTimes;
 
+            Logic.Logging.Logger.Write($"All accounts are blocked. None of your accounts are available to switch to, so bot will sleep for {pauseTime} minutes until next account is available to run.");
             if (session.LogicSettings.NotificationConfig.EnablePushBulletNotification == true)
                 PushNotificationClient.SendNotification(session, "All accounts are blocked.", $"None of your accounts are available to switch to, so bot will sleep for {pauseTime} minutes until next account is available to run.", true).ConfigureAwait(false);
 
